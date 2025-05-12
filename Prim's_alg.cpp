bc0c9c47f9ea7234021fb95fd6866801a2c59680
Nziza-Prince <nzizaprince7@gmail.com>
2025-05-12 20:02:50 +0200

    implemented prim's from scratch and added a .gitignore

--- Prim's_alg.cpp
+++ Prim's_alg.cpp
@@ -3,26 +3,32 @@
 #include <climits>
 using namespace std;
 
-void PrimMST(vector<vector<int>>& graph, int V) {
-    vector<int> parent(V, -1), key(V, INT_MAX);
+void PrimMST(vector<vector<int>>& graph, int V){
+    vector<int> parent(V, -1),key(V, INT_MAX);
     vector<bool> inMST(V, false);
     key[0] = 0;
 
-    for (int count = 0; count < V - 1; count++) {
-        int u = -1;
-        for (int v = 0; v < V; v++)
-            if (!inMST[v] && (u == -1 || key[v] < key[u]))
-                u = v;
 
-        inMST[u] = true;
-        for (int v = 0; v < V; v++)
-            if (graph[u][v] && !inMST[v] && graph[u][v] < key[v])
-                parent[v] = u, key[v] = graph[u][v];
+    for(int count = 0; count < V-1 ; count++){
+       int u = -1;
+       for(int v = 0; v < V ; v++){
+        if(!inMST[v] && (u == -1 || key[v] < key[u])){
+            u =v;
+            inMST[u] = true; 
+        }
+    }
+    
+        for(int v = 0; v < V; v++){
+            if(graph[u][v] && !inMST[v] && graph[u][v] < key[v]){
+                parent[v] = u , key[v] = graph[u][v];
+            }
+        }
     }
 
     cout << "Prim's MST Edges:\n";
-    for (int i = 1; i < V; i++)
-        cout << parent[i] << " - " << i << " : " << graph[i][parent[i]] << "\n";
+    for(int i = 1;i < V;i++){
+        cout << parent[i] << " - " << i << " : "<< graph[i][parent[i]] << "\n";
+    }
 }
 
 int main() {
